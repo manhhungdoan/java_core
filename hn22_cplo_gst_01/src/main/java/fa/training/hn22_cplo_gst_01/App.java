99cdef523dfc7b2f07451a294b9486136cfb4592
mhung <hungthiendoan@gmail.com>
2022-09-13 02:20:02 +0700

    first commit

---
+++ hn22_cplo_gst_01/src/main/java/fa/training/hn22_cplo_gst_01/App.java
@@ -0,0 +1,13 @@
+package fa.training.hn22_cplo_gst_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ hn22_cplo_gst_01/src/test/java/fa/training/hn22_cplo_gst_01/AppTest.java
@@ -0,0 +1,20 @@
+package fa.training.hn22_cplo_gst_01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
